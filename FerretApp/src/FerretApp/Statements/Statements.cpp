25bd9fd6402dd0f729d02808a590f898ef37dbc0
Anythingsoup01 <williamenglish1220@gmail.com>
2026-07-05 02:04:37 -0400

    Income Statement
    Income Statement: The Income statement is now working as intended, all
    tables marked as income will be included as income, and all tables
    marked as expense will be included as expenses

--- FerretApp/src/FerretApp/Statements/Statements.cpp
+++ FerretApp/src/FerretApp/Statements/Statements.cpp
@@ -4,18 +4,13 @@
 
 #include "FerretApp/Utils/Utils.h"
 
-#include <format>
-
 namespace Ferret {
 
 void Statements::OnRenderData() {
-  if (m_RenderIncome) {
-    OnRenderIncome();
-  }
+  OnRenderIncome();
 }
 
 void Statements::OnRenderIncome() {
-  // TODO: Cache and Serialize these!
   std::vector<AccountTable> incomeTables;
   std::vector<AccountTable> expenseTables;
 
@@ -23,6 +18,7 @@ void Statements::OnRenderIncome() {
   float expenseAccountsTotal = 0;
 
   int largestAccountNameSize = 0;
+  int largestAmountSize = 0;
 
   for (auto &[id, table] : Ledger::GetTables()) {
     switch (table.GetTracking()) {
@@ -43,21 +39,31 @@ void Statements::OnRenderIncome() {
     if (largestAccountNameSize < accountNameSize) {
       largestAccountNameSize = accountNameSize;
     }
+
+    int amountSize = Utils::GetDigitCount((int)(table.GetBalance() * 100)) + 3;
+    if (largestAmountSize < amountSize) {
+      largestAmountSize = amountSize;
+    }
   }
 
-  std::string fmt = "{0" + std::to_string(largestAccountNameSize * 3) + "}";
-  std::string formattedStr = fmt::format(, "#");
-  ImVec2 entrySize = ImGui::CalcTextSize(formattedStr.c_str());
-  ImGuiTableFlags flags = ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg | ImGuiTableFlags_BordersV | ImGuiTableFlags_SizingStretchProp;
+  ImVec2 typeCol = ImGui::CalcTextSize("Net Income  ");
+  std::string formattedStr = fmt::format("{:0{}}", 0, largestAccountNameSize);
+  std::string incomeStatementStr = "Income Statement";
+  ImVec2 accountCol = formattedStr.length() > incomeStatementStr.length() ? ImGui::CalcTextSize(formattedStr.c_str()) : ImGui::CalcTextSize(incomeStatementStr.c_str());
+  formattedStr = fmt::format("{:0{}}", 0, largestAmountSize);
+  ImVec2 amountCol = ImGui::CalcTextSize(formattedStr.c_str());
+
+  ImVec2 entrySize = ImVec2(typeCol.x + accountCol.x + amountCol.x, typeCol.y);
 
   ImVec2 tableSize = ImVec2(entrySize.x, (incomeTables.size() + expenseTables.size() + 8) * entrySize.y); // 4 being headers, Income Statement, Income, Expense + 2 for dummy items + 1 for total
 
   float total = 0;
+  ImGuiTableFlags flags = ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg | ImGuiTableFlags_BordersV | ImGuiTableFlags_SizingStretchProp;
   ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(0, 0));
   if (ImGui::BeginTable("##IncomeStatement", 3, flags, tableSize)) {
-    ImGui::TableSetupColumn("##EmptyHeader", ImGuiTableColumnFlags_WidthFixed, tableSize.x / 3.0f);
-    ImGui::TableSetupColumn("Income Statement", ImGuiTableColumnFlags_WidthFixed, tableSize.x / 3.0f);
-    ImGui::TableSetupColumn("##EmptyHeader", ImGuiTableColumnFlags_WidthFixed, tableSize.x / 3.0f);
+    ImGui::TableSetupColumn("##EmptyHeader", ImGuiTableColumnFlags_WidthFixed, typeCol.x);
+    ImGui::TableSetupColumn("Income Statement", ImGuiTableColumnFlags_WidthFixed, accountCol.x);
+    ImGui::TableSetupColumn("##EmptyHeader", ImGuiTableColumnFlags_WidthFixed, amountCol.x);
     Utils::HeaderCentered(3);
 
     ImGui::TableNextRow();
@@ -94,7 +100,7 @@ void Statements::OnRenderIncome() {
     for (auto &table : expenseTables) {
       ImGui::TableNextRow();
       ImGui::TableSetColumnIndex(1);
-      ImGui::Text("%d", table.GetAccountNumber());
+      ImGui::Text("%s (%d)", table.GetName().c_str(), table.GetAccountNumber());
       ImGui::TableSetColumnIndex(2);
       ImGui::Text("%.2f", table.GetBalance());
     }
@@ -126,9 +132,6 @@ void Statements::OnRenderIncome() {
   }
 
   ImGui::PopStyleVar();
-  // |  Income Statement  |
-  // | Income |     |     |
-  // |        | 101 | AMT |
 }
 
 }
--- FerretApp/src/FerretApp/Statements/Statements.h
+++ FerretApp/src/FerretApp/Statements/Statements.h
@@ -7,8 +7,6 @@ public:
   static void OnRenderData();
 private:
   static void OnRenderIncome();
-private:
-  static inline bool m_RenderIncome = true;
 };
 
 }
